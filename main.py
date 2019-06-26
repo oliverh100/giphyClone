2f308d4b51529f4972fefe13bd77f27dca0bf6e1
Oliver Herskovits <oliverh100@gmail.com>
2019-06-26 10:43:30 +0100

    next commit

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import random
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return render_template('index.html', title = 'index')
 
-@app.route('/home/')
+@app.route('/')
 def home():
 	rand = random.randint(1,10)
 	return render_template('home.html', title = 'home', num = rand, gif = "/static/library/" + str(rand) + ".gif", data  = '')
 
-@app.route('/home/', methods = ['POST'])
-def home_data():
-	data = request.form['entry']
+
+@app.route('/random/')
+def randomGIF():
 	rand = random.randint(1,10)
-	return render_template('home.html', title = 'home', num = rand, gif = "/static/library/1.gif", data = data)
+	return render_template('random.html', title = 'random', num = rand, gif  = "/static/library/" + str(rand) + ".gif")
+
+
+@app.route('/search/')
+def search():
+	return render_template('search.html', title = 'search', data = '')
+
+
+@app.route('/search/', methods = ['POST'])
+def searchForm():
+	data = request.form['entry']
+	return render_template('search.html', title = 'search', data = data)
