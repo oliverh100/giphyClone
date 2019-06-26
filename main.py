fb9e048439fd08d0b9fd0fee85ba0919d06379c6
Oliver Herskovits <oliverh100@gmail.com>
2019-06-26 11:26:30 +0100

    next commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, flash
 import random
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '3d6f45a5fc12445dbac2f59c3b6c7cb1'
+
+gifs = {'dog': '1', 'pizza': '2', 'yawning': '3'}
 
 
 @app.route('/')
@@ -17,10 +20,16 @@ def randomGIF():
 
 @app.route('/search/')
 def search():
-	return render_template('search.html', title = 'search', data = '')
+	return render_template('search.html', title = 'search', data = '', gif = '')
 
 
 @app.route('/search/', methods = ['POST'])
 def searchForm():
 	data = request.form['entry']
-	return render_template('search.html', title = 'search', data = data)
+	if data.lower() not in gifs:
+		return render_template('search.html', title = 'search', data = data, flag = 'True', gif = '')
+		print('ERROR')
+	else:
+		num = gifs[data]
+		return render_template('search.html', title = 'search', data = data, flag = 'False', gif = "/static/library/" + num + ".gif")
+	return render_template('search.html', title = 'search', data = data, flag = 'False', gif = '')
