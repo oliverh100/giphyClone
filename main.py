3d9f7437a3c31a8dfeb690d35617ccc035345a2c
Oliver Herskovits <oliverh100@gmail.com>
2019-06-26 13:17:00 +0100

    next commit

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
 from flask import Flask, render_template, request, url_for, flash
-import random
+from random import randint
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3d6f45a5fc12445dbac2f59c3b6c7cb1'
+
 
 gifs = {'dog': '1', 'pizza': '2', 'yawning': '3'}
 
 
-@app.route('/')
+
+@app.route('/', methods = ['GET'])
 def home():
-	rand = random.randint(1,10)
-	return render_template('home.html', title = 'home', num = rand, gif = "/static/library/" + str(rand) + ".gif", data  = '')
-
-
-@app.route('/random/')
-def randomGIF():
-	rand = random.randint(1,10)
-	return render_template('random.html', title = 'random', num = rand, gif  = "/static/library/" + str(rand) + ".gif")
-
-
-@app.route('/search/')
-def search():
-	return render_template('search.html', title = 'search', data = '', gif = '')
-
-
-@app.route('/search/', methods = ['POST'])
-def searchForm():
-	data = request.form['entry']
-	if data.lower() not in gifs:
-		return render_template('search.html', title = 'search', data = data, flag = 'True', gif = '')
-		print('ERROR')
+	data = request.args.get('entry')
+	if data:
+		if data.lower() not in gifs:
+			return render_template('home.html', flag = 'True', gif = '')
+		else:
+			num = gifs[data]
+			return render_template('home.html', flag = 'False', gif = "/static/library/" + num + ".gif")
 	else:
-		num = gifs[data]
-		return render_template('search.html', title = 'search', data = data, flag = 'False', gif = "/static/library/" + num + ".gif")
-	return render_template('search.html', title = 'search', data = data, flag = 'False', gif = '')
+		rand = randint(1,10)
+		return render_template('home.html', flag = 'False', gif = "/static/library/" + str(rand) + ".gif")
